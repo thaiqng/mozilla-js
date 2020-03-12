07847cdc9e310a798cd9706af13a03d92b88579f
Thai Nguyen <developers@buscoro.com>
2020-03-12 16:44:15 -0600

    add click to change img func

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,1 +1,13 @@
 document.querySelector('h1').textContent = 'Hello World!'; // DOM API
+
+let myImage = document.querySelector('img')
+
+myImage.onclick = function() {
+  let mySource = myImage.getAttribute('src') // get attribute to check
+  if(mySource === 'images/firefox-icon.png'){
+    myImage.setAttribute('src', 'images/bigeye.png') // set attribute
+  }
+  else{
+    myImage.setAttribute('src', 'images/firefox-icon')
+  }
+}
